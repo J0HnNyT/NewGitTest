8b6f14773b707bc8fccfb2a5fc5de396dbddee7b
Johnny Taylor <J0HnNyT@users.noreply.github.com>
2018-04-22 13:22:27 -0700

    added HelloWorld Java file and folder

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+*Testing out GitHub
+ */
+package helloworld;
+
+/**
+ *
+ * @author Johnny
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub World!");
+    }
+    
+}
